2c974153e5541f17e61cb9f14e321e44c0fc853b
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-12-14 16:36:55 +0800

    feat(update)

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,11 +1,11 @@
 // import ModalWrapper from '@/widgets/ModalWrapper'
 // import ModalPrompt from '@/widgets/ModalPrompt'
-import ModalWrapper from 'widgets/ModalWrapper'
+// import ModalWrapper from 'widgets/ModalWrapper'
 import ModalPrompt from 'widgets/ModalPrompt'
 
 export default {
   install (Vue) {
-    Vue.use(ModalWrapper)
+    // Vue.use(ModalWrapper)
     Vue.use(ModalPrompt)
   }
 }
